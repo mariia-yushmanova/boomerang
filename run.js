a5af87d376fb9ff10f9100721ea0f1fa5dd53cb6
Ibra <Ibrahim-1991@mail.ru>
2022-12-09 12:56:38 +0300

    check connection db

--- run.js
+++ run.js
@@ -1,12 +1,24 @@
 // Основной файл.
 // Запускает игру.
 const Game = require('./src/Game');
+const db = require('./db/models')
+const { Sequelize } = require("sequelize");
 
 // Инициализация игры с настройками.
 const game = new Game({
   trackLength: 30,
 });
 
-
 // Запуск игры.
 game.play();
+
+// async function connectCheck() {
+//   try {
+//     await db.sequelize.authenticate();
+//     console.log("База успешно подключена!");
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// connectCheck();
