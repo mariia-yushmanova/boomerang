06e0522ef4b6b3c94f23c8562d3ceabd7655000a
Slimshady3133 <i.puzakov2010@mail.ru>
2022-12-11 20:36:48 +0300

    replace View

--- src/Game.js
+++ src/Game.js
@@ -2,21 +2,21 @@
 // Или можно не импортировать,
 // а передавать все нужные объекты прямо из run.js при инициализации new Game().
 
-const Hero = require("./game-models/Hero");
-const Enemy = require("./game-models/Enemy");
-const Boomerang = require("./game-models/Boomerang");
-const View = require("./View");
-const runInteractiveConsole = require("./keyboard");
+const Hero = require('./game-models/Hero');
+const Enemy = require('./game-models/Enemy');
+const Boomerang = require('./game-models/Boomerang');
+const View = require('./View');
+const runInteractiveConsole = require('./keyboard');
 
-const { Sequelize, sequelize, scores } = require("../db/models");
-const db = require("../db/models");
+const { Sequelize, sequelize, scores } = require('../db/models');
+const db = require('../db/models');
 
 (async () => {
   try {
     await db.sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error('Unable to connect to the database:', error);
   }
 })();
 
@@ -50,7 +50,7 @@ class Game {
   regenerateTrack() {
     // Сборка всего необходимого (герой, враг(и), оружие)
     // в единую структуру данных
-    this.track = new Array(this.trackLength).fill(" ");
+    this.track = new Array(this.trackLength).fill(' ');
     this.track[this.hero.position] = this.hero.skin;
     this.track[this.enemy.position] = this.enemy.skin;
     this.track[this.boomerang.position] = this.boomerang.skin;
@@ -63,17 +63,17 @@ class Game {
       this.hero.position = 0;
       this.hero.livesCount -= 1;
       if (this.hero.livesCount === 2) {
-        this.hero.health = "Жизни: 💜💜🖤";
+        this.hero.health = 'Жизни: 💜💜🖤';
         this.enemy.position = 29;
       }
       if (this.hero.livesCount === 1) {
-        this.hero.health = "Жизни: 💜🖤🖤";
+        this.hero.health = 'Жизни: 💜🖤🖤';
         this.enemy.position = 29;
       }
       if (this.hero.livesCount === 0) {
-        this.hero.health = "Жизни: 🖤🖤🖤";
+        this.hero.health = 'Жизни: 🖤🖤🖤';
         this.enemy.position = 29;
-        this.hero.skin = "💀";
+        this.hero.skin = '💀';
         // setTimeout(() => {
         this.hero.die();
         await this.name();
@@ -88,23 +88,23 @@ class Game {
       this.enemy = new Enemy();
       this.enemy.position = 29;
       this.enemy.moveLeft();
-      this.boomerang.direction = "left";
+      this.boomerang.direction = 'left';
     }
 
     if (this.boomerang.position <= this.hero.position) {
       this.boomerang.position = undefined;
-      this.boomerang.direction = "right";
+      this.boomerang.direction = 'right';
       // this.boomerang = new Boomerang();
     }
     if (
       this.boomerang.position !== this.hero.position &&
-      this.boomerang.direction === "right"
+      this.boomerang.direction === 'right'
     ) {
       this.boomerang.moveRight();
     }
     if (
       this.boomerang.position !== this.hero.position &&
-      this.boomerang.direction === "left"
+      this.boomerang.direction === 'left'
     ) {
       this.boomerang.moveLeft();
     }
